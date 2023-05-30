a7623f71be03bde1c7fe00b920df49d7ed7586de
Eometheous <jonathan.sthomas@icloud.com>
2023-05-29 17:25:42 -0700

    Initial Commit

---
+++ FirstCalendarInCpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FirstCalendarInCpp
+//
+//  Created by Jonathan Thomas on 5/29/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
