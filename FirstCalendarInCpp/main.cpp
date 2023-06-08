121f09d0bda9b5ce22d0260c4d60bfd621fd5ef6
Eometheous <jonathan.sthomas@icloud.com>
2023-06-08 12:06:23 -0700

    now uses tm variables and overloaded << TimeInterval operator

--- FirstCalendarInCpp/main.cpp
+++ FirstCalendarInCpp/main.cpp
@@ -5,16 +5,27 @@
 //  Created by Jonathan Thomas on 5/29/23.
 //
 
-#include <iostream>
 #include "TimeInterval.hpp"
-#include "Time.hpp"
 
 int main(int argc, const char * argv[]) {
-    Time time1(5, 0, 0);
-    Time time2(10,0,0);
+    tm *time1 = new tm;
+    tm *time2 = new tm;
     
-    TimeInterval timeInterval(time1, time2);
+    time1->tm_hour = 5;
+    time1->tm_min = 0;
+    time1->tm_sec = 0;
     
-    std::cout << timeInterval.toString();
+    time2->tm_hour = 10;
+    time2->tm_min = 0;
+    time2->tm_sec = 0;
+    
+    TimeInterval *time_interval = new TimeInterval(*time1, *time2);
+    
+    cout << *time_interval << endl;
+    
+    delete time1;
+    delete time2;
+    delete time_interval;
+    
     return 0;
 }
