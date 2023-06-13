f1ee130e2ac443064cfccc6d308224c613ef65b2
Eometheous <jonathan.sthomas@icloud.com>
2023-06-12 21:42:27 -0700

    Main testing new changes

--- FirstCalendarInCpp/main.cpp
+++ FirstCalendarInCpp/main.cpp
@@ -8,23 +8,13 @@
 #include "TimeInterval.hpp"
 
 int main(int argc, const char * argv[]) {
-    tm *time1 = new tm;
-    tm *time2 = new tm;
-    
-    time1->tm_hour = 5;
-    time1->tm_min = 0;
-    time1->tm_sec = 0;
-    
-    time2->tm_hour = 10;
-    time2->tm_min = 0;
-    time2->tm_sec = 0;
-    
-    TimeInterval *time_interval = new TimeInterval(*time1, *time2);
-    
-    cout << *time_interval << endl;
-    
-    delete time1;
-    delete time2;
+    Time time1, time2;
+    cout << "Enter start time (xx:xx:xx): ";
+    cin >> time1;
+    cout << "Enter end time (xx:xx:xx): ";
+    cin >> time2;
+    TimeInterval *time_interval = new TimeInterval(time1, time2);
+    cout << "Entered Time Interval: " << *time_interval << endl;
     delete time_interval;
     
     return 0;
